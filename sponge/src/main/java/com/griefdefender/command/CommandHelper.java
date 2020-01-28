5c466827aa08afce4c2cf263aeb3ef6f8a67c548
Hutchy <hutchy5225@gmail.com>
2020-01-28 23:38:38 +1100

    Allowed 'any' command context to match all commands from a plugin. e.g. nucleus:any

--- sponge/src/main/java/com/griefdefender/command/CommandHelper.java
+++ sponge/src/main/java/com/griefdefender/command/CommandHelper.java
@@ -965,6 +965,8 @@ public class CommandHelper {
 
     private static boolean validateCommandMapping(CommandSource src, String command, String pluginId) {
         CommandMapping commandMapping = Sponge.getCommandManager().get(command).orElse(null);
+        if (command.equals("any"))
+            return true;
         if (commandMapping == null) {
             TextAdapter.sendComponent(src, MessageStorage.MESSAGE_DATA.getMessage(MessageStorage.PLUGIN_COMMAND_NOT_FOUND,
                     ImmutableMap.of(
