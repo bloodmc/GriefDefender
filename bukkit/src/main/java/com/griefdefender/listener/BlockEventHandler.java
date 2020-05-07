53176a7f908575595d6e7d216d51a7f43092f0c7
bloodshot <jdroque@gmail.com>
2020-05-07 03:27:31 -0400

    Allow pistons to be protected in wilderness.

--- bukkit/src/main/java/com/griefdefender/listener/BlockEventHandler.java
+++ bukkit/src/main/java/com/griefdefender/listener/BlockEventHandler.java
@@ -152,9 +152,6 @@ public class BlockEventHandler implements Listener {
                 // Sticky pistons will attach to next block so we need to check it
                 location = BlockUtil.getInstance().getBlockRelative(location, event.getDirection());
                 targetClaim = this.storage.getClaimAt(location, targetClaim);
-                if (targetClaim.isWilderness()) {
-                    return;
-                }
             }
 
             if (handleBlockBreak(event, location, targetClaim, event.getBlock(), location.getBlock(), user, false)) {
